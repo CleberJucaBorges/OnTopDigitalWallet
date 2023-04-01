7c176504ac901dc7a220f22a7e0561ac366106c3
CleberJucaBorges <79108017+CleberJucaBorges@users.noreply.github.com>
2023-04-01 18:12:08 -0300

    improving error messages for @Valid on controllers

---
+++ src/main/java/com/ontopchallenge/ontopdigitalwallet/Controller/ExceptionHandlerController.java
@@ -0,0 +1,23 @@
+package com.ontopchallenge.ontopdigitalwallet.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        String errorMessage = bindingResult.getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
+}
+
